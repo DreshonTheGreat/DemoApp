c85e08eb114666c6c91e5e362314a9473788a790
DreshonTheGreat <110046041+DreshonTheGreat@users.noreply.github.com>
2022-07-26 10:57:58 -0400

    Add files via upload

---
+++ ApplicationInsightsTutorialLogs-master/ApplicationInsightsTutorial/Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+
+namespace ApplicationInsightsTutorial
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateWebHostBuilder(
+   string[] args) => Host.CreateDefaultBuilder(args)
+      .ConfigureWebHostDefaults(webBuilder =>
+      {
+          webBuilder.UseStartup<Startup>();
+      })
+      .ConfigureLogging(logging =>
+      {
+          // clear default logging providers
+          logging.ClearProviders();
+
+          // add built-in providers manually, as needed 
+          logging.AddConsole();
+          logging.AddDebug();
+          logging.AddEventLog();
+          logging.AddEventSourceLogger();
+      });
+
+    }
+}
